c6bbdb80dc5e29f81061f44baac0137663cdabc6
swapnilwarkar@gmail.com <>
2024-04-29 17:36:15 +0530

    checkin the features - Added nodejs_crashcourse directory

--- git_checkin.sh
+++ git_checkin.sh
@@ -1,7 +1,7 @@
 git branch
 git status 
 git add . 
-git commit -m "checkin the features - Added key_gen directory" 
+git commit -m "checkin the features - Added nodejs_crashcourse directory" 
 git push origin main 
 git status
 git log -n2
