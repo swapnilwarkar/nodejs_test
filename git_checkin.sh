a7d6a3447a811453578b2272c45912bb7fc9df24
swapnilwarkar@gmail.com <>
2024-04-29 18:02:25 +0530

    checkin the features - Removed the git folder from sub directory

--- git_checkin.sh
+++ git_checkin.sh
@@ -1,7 +1,7 @@
 git branch
 git status 
 git add . 
-git commit -m "checkin the features - Added nodejs_crashcourse directory" 
+git commit -m "checkin the features - Removed the git folder from sub directory" 
 git push origin main 
 git status
 git log -n2
